f3a81a0dbad314a8ac43b1689eccef5f0685e9ce
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2024-02-11 20:12:43 +0700

    aa

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('phone')->nullable();
             $table->text('address')->nullable();
             $table->string('email')->unique();
+            $table->string('status')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
